080fe0b2ed50066e9c8af0c6fa99b0d1ee05d72e
Andres Castellanos <andres.about@gmail.com>
2024-08-16 16:58:56 -0500

    cli commands fixed for the new monorepo arquitecture

--- cli/copy-toasty.ts
+++ cli/copy-toasty.ts
@@ -7,7 +7,10 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const srcPath = path.resolve(__dirname, "..", "components", "main.tsx");
+const srcPath = path.resolve(
+  __dirname,
+  "../../visual-testing/src/components/main.tsx",
+);
 const destPath = path.resolve(process.cwd(), "src/components/TestToasty.tsx");
 
 // Crear la carpeta src/components si no existe
