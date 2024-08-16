d037d269b29ac2bc82e9fc02879df1cb65108cce
Andres Castellanos <andres.about@gmail.com>
2024-08-16 10:59:57 -0500

    cli command to overwrite the tailwind configuration

--- src/cli/copy-toasty.js
+++ src/cli/copy-toasty.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // Obtener el directorio del archivo actual
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const srcPath = path.resolve(__dirname, '..', 'components' ,'main.tsx');
-const destPath = path.resolve(process.cwd(), 'src/components/TestToasty.tsx');
+const srcPath = path.resolve(__dirname, "..", "components", "main.tsx");
+const destPath = path.resolve(process.cwd(), "src/components/TestToasty.tsx");
 
 // Crear la carpeta src/components si no existe
 fs.mkdirSync(path.dirname(destPath), { recursive: true });
@@ -16,4 +16,4 @@ fs.mkdirSync(path.dirname(destPath), { recursive: true });
 // Copiar el archivo
 fs.copyFileSync(srcPath, destPath);
 
-console.log('Component TestToasty copied to src/components/');
+console.log("Component TestToasty copied to src/components/");
--- src/cli/setup-tailwind.ts
+++ src/cli/setup-tailwind.ts
@@ -1,14 +1,58 @@
-import { writeFile } from "fs/promises";
+#!/usr/bin/env node
+
+import fs from "fs/promises";
+
+const tailwindConfig = `
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        trackingProgress: {
+          "0%": { width: "100%" },
+          "100%": { width: "0%" },
+        },
+      },
+      animation: {
+        trackingProgress: "trackingProgress 1s linear forwards",
+      },
+      colors: {
+        primary: "#1B1B1B",
+        secondary: "#EDEDED",
+        background: "#FFFFFF",
+        text: "#000000",
+        border: "#B3B2B2",
+
+        warning: "#DC7609",
+        error: "#E60000",
+        info: "#0973DC",
+      },
+    },
+    borderRadius: {
+      "200": "0.25rem",
+      "400": "0.5rem",
+      full: "1000px",
+    },
+  },
+  plugins: [],
+};
+export default config;
+`;
 
 async function writeToFile() {
-  const data = "Hello, world!";
-  const filePath = "./example.txt";
+  const filePath = "./tailwind.config.ts";
 
   try {
-    await writeFile(filePath, data, "utf8");
-    console.log("File written successfully");
+    await fs.writeFile(filePath, tailwindConfig, "utf8");
+    console.log("Tailwind config was overwritten");
   } catch (error) {
-    console.error("Error writing file:", error);
+    console.error("Error overwriting tailwind config:", error);
   }
 }
 
