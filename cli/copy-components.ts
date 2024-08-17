b2a2f33941f91db6f373cb3fe4f6b4c5be96c0bf
Andres Castellanos <andres.about@gmail.com>
2024-08-17 10:16:27 -0500

    copy-components cli command now copies types and hooks

--- cli/copy-components.ts
+++ cli/copy-components.ts
@@ -3,25 +3,52 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Obtener el directorio del archivo actual
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const srcDirectory = "../../visual-testing/src/";
 
-const srcDir = path.resolve(__dirname, "../../visual-testing/src/components/");
-const destDir = path.resolve(process.cwd(), "src/components/");
+const copyDirectories = [
+  {
+    src: path.resolve(__dirname, srcDirectory, "components/"),
+    dest: path.resolve(process.cwd(), "src/components/"),
+    filterCb: (file: string) =>
+      file.endsWith(".tsx") && !file.includes(".stories.tsx"),
+  },
+  {
+    src: path.resolve(__dirname, srcDirectory, "types/"),
+    dest: path.resolve(process.cwd(), "src/types/"),
+  },
+  {
+    src: path.resolve(__dirname, srcDirectory, "hooks/"),
+    dest: path.resolve(process.cwd(), "src/hooks/"),
+  },
+];
 
-// Crear la carpeta src/components si no existe
-fs.mkdirSync(destDir, { recursive: true });
+function copyAll(
+  src: string,
+  dest: string,
+  filterCb?: (file: string) => boolean,
+) {
+  // Read files and apply filter if needed
+  const filteredFiles = filterCb
+    ? fs.readdirSync(src).filter(filterCb)
+    : fs.readdirSync(src);
 
-// Leer todos los archivos .tsx en el directorio fuente
-const files = fs
-  .readdirSync(srcDir)
-  .filter((file) => file.endsWith(".tsx") && !file.includes(".stories.tsx"));
+  // Copy all files into destination
+  filteredFiles.forEach((file) => {
+    const srcPath = path.join(src, file);
+    const destPath = path.join(dest, file);
+    fs.copyFileSync(srcPath, destPath);
+    console.log(`Component ${file} copied to src/components/`);
+  });
+}
 
-// Copiar cada archivo .tsx al directorio de destino
-files.forEach((file) => {
-  const srcPath = path.join(srcDir, file);
-  const destPath = path.join(destDir, file);
-  fs.copyFileSync(srcPath, destPath);
-  console.log(`Component ${file} copied to src/components/`);
+function ensureOrCreate(dest: string) {
+  if (fs.existsSync(dest)) return;
+  fs.mkdirSync(dest, { recursive: true });
+}
+
+copyDirectories.forEach(({ src, dest, filterCb }) => {
+  ensureOrCreate(dest);
+  copyAll(src, dest, filterCb);
 });
