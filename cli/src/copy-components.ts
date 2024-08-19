90fc899b620f77606a5a673f73f0a21b9ea6b746
Andres Castellanos <andres.about@gmail.com>
2024-08-19 15:18:32 -0500

    assets folder for all the needed files for the cli commands

--- cli/src/copy-components.ts
+++ cli/src/copy-components.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const srcDirectory = "../../visual-testing/src";
+const srcDirectory = "../assets/";
 
 type CopyStatement = {
   src: string;
